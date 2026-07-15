2da966277db07cee6ad42ff66980821984c58c16
Tanish Jain <tanishjain020205@gmail.com>
2026-07-15 15:57:14 +0530

    chore: update playwright webServer command to use production build for testing

--- playwright.config.mjs
+++ playwright.config.mjs
@@ -16,7 +16,7 @@ export default defineConfig({
     trace: "on-first-retry",
   },
   webServer: {
-    command: "npm run dev",
+    command: "npm run build && npm run start",
     url: "http://127.0.0.1:3000",
     reuseExistingServer: !process.env.CI,
     timeout: 120_000,
